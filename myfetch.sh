22c785925deb02ef516c7759f1edaf9d15c8aeb4
Tas Long <bepsii.sh@gmail.com>
2018-04-07 19:38:17 -0400

    added distro support

--- myfetch.sh
+++ myfetch.sh
@@ -1,7 +1,25 @@
+PATH=$PATH:~/etc
+export PATH
 kernal_name=$(uname -s)
+distroID=$(cat /etc/*release | grep -w ID )
 case $kernal_name in
 	"Linux" | "GNU" )
 		os="Linux"
+		#distros are just checking distro id for the distro name
+			case $distroID in  
+			       	*"debian"* )
+					distro="Debian"
+					;;
+				*"ubuntu"* )
+					distro="Ubuntu"
+					;;
+				*"opensuse"* )
+					distro="openSUSE"
+					;;
+				*)
+					distro="Unknown, Please contact me on github for support of your distro"
+					;;
+			esac
 		;;
 	"CYGWIN"* | "MYSY"* | "MINGW"* )
 		os="Windows"
@@ -10,4 +28,6 @@ case $kernal_name in
 		os="Unknown"
 		;;
 esac
+echo "$distroID"
 echo "$os"
+echo "$distro"
